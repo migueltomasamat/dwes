c602be1405a143a4ff953d4a191e7e044a3c6915
Miguel A. Tomás <migueltomas.informatica@iespacomolla.es>
2022-08-11 19:29:16 +0200

    Ejercicio 201

---
+++ Tema2/201tresfrases.php
@@ -0,0 +1,28 @@
+
+<?php
+
+/*
+201tresfrases.php: Muestra 3 frases, cada una en un párrafo utilizando las tres posibilidades que existen de mostrar contenido. 
+Tras ello, introduce dos comentarios, uno de bloque y otro de una línea.
+*/
+
+echo "Frase1 <br>";
+
+print "Frase2 <br>";
+
+?>
+
+<?="Frase3 <br>"; ?>
+
+<?php 
+
+//Comentario en línea
+
+/* 
+Comentario 
+en 
+bloque 
+*/
+
+
+
